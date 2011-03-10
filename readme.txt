40994129e43fa6a29695abd3b0c9180a8df9e0d1
davidrapson <davidrapson@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-10 14:18:05 +0000

    Intial svn commit
    git-svn-id: https://plugins.svn.wordpress.org/tinymce-mailto/trunk@358257 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,31 @@
+=== TinyMCE MailTo ===
+Contributors: davidrapson
+Tags: wysiwyg, tinymce, email, mail, mailto
+Requires at least: 2.9
+Tested up to: 3.1
+Stable tag: 1.0
+
+Adds a button to the WordPress visual editor which converts the selected text into a mailto link.
+
+== Description ==
+
+This plugin adds a buttons to [TinyMCE](http://tinymce.moxiecode.com/) which converts the selected text into a mailto link.
+
+**Language Support:** English.
+
+== Installation ==
+
+1. Download.
+2. Unzip.
+3. Upload to the plugins directory (wp-content/plugins).
+4. Activate the plugin.
+
+== Changelog ==
+
+= 1.0 =
+Initial release.
+
+== Upgrade Notice ==
+
+= 1.0 =
+Initial release.
---
+++ tinymce-mailto-buttons.php
@@ -0,0 +1,58 @@
+<?php
+/*
+
+Plugin Name: TinyMCE MailTo
+Description: Adds a button to the visual editor which converts the selected text into a mailto link.
+Version: 1.0
+Author: Colebridge Communications
+Author URI: http://colebridge.org/
+
+TinyMCE MailTO. Adds a button to the visual editor which converts the selected text into a mailto link.
+Copyright (C) 2011 David Rapson, Colebridge Communications
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>./copyleft/gpl.html
+
+
+*/
+
+function tinymce_mailto_addbuttons()
+{
+
+  // Don't bother doing this stuff if the current user lacks permissions
+  if ( current_user_can('edit_posts') && current_user_can('edit_pages') ) {
+    if ( get_user_option('rich_editing') == 'true') {
+      add_filter("mce_external_plugins", "add_tinymce_mailto_plugin");
+      add_filter('mce_buttons', 'register_tinymce_mailto_buttons');
+    }
+  }
+
+}
+
+
+function register_tinymce_mailto_buttons($buttons)
+{
+  array_push($buttons, "separator", "mailto");
+  return $buttons;
+}
+
+
+// Load the TinyMCE plugin : editor_plugin.js (wp2.5)
+function add_tinymce_mailto_plugin($plugin_array)
+{
+
+  $plugin_name = preg_replace('/\.php/','',basename(__FILE__));
+  $plugin_array['mailto'] = WP_PLUGIN_URL .'/'.$plugin_name.'/mce/mailto/editor_plugin.dev.js';
+  return $plugin_array;
+}
+add_action('init', 'tinymce_mailto_addbuttons');
